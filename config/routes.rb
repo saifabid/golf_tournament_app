f9f1276d19b1415e7c33d3edd835f1449a7819d7
Pranav Mehndiratta <pranav.mehndiratta@mail.utoronto.ca>
2016-09-26 18:31:47 -0400

    Missed modified routes file, with Vaibhav

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
 
-  get 'users/new'
+  # get 'users/new'
 
-  get 'users/create'
+  # get 'users/create'
 
-  get 'users/show'
+  # get 'users/show'
 
-  get 'users/edit'
+  # get 'users/edit'
 
-  get 'users/update'
+  # get 'users/update'
 
-  get 'users/destroy'
+  # get 'users/destroy'
 
-  get 'tournament_registration/index'
+  # get 'tournament_registration/index'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#hello_world'
