40d8505ffe6f9012bdfc8686c6c9ccafafc07b7e
Pranav Mehndiratta <pranav.mehndiratta@mail.utoronto.ca>
2016-09-26 11:07:12 -0400

    Generated initial User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    # @users = User.all
+  end
+
+  def new
+    # @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   get 'tournament_registration/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
