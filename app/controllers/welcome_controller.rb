7878fd58b6f0353796eb8106b0ea5d3da3d4947f
Iddo <tobermory.sun@gmail.com>
2016-09-30 00:59:55 -0400

    Event sorting using dropdown and form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,20 @@ require 'date'
 
 class WelcomeController < ApplicationController
   def hello_world
-  	@tournaments = Tournament.where("start_date >= NOW()").order(start_date: :asc)
+
+  	sortType = params[:q]
+
+  	if  sortType == "1"
+	  	@tournaments = Tournament.where("start_date >= NOW()").order(start_date: :asc)
+  	elsif sortType == "2"
+  		@tournaments = Tournament.where("start_date >= NOW()").order(start_date: :desc)
+  	elsif sortType == "3"
+  		@tournaments = Tournament.where("start_date >= NOW()").order(title: :asc)
+  	elsif sortType == "4"
+  		@tournaments = Tournament.where("start_date >= NOW()").order(title: :desc)
+  	else
+	  	@tournaments = Tournament.where("start_date >= NOW()").order(start_date: :asc)
+	end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'tournament_registration/index'
 
+  get 'welcome' => 'welcome#hello_world'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#hello_world'
+
 end
