4e0822dbe8c323fb512b0a8aa72b5f1d16838d71
abidsaif <saif.abid@mail.utoronto.ca>
2016-10-04 22:18:50 -0400

    Added tournament_params and image_store_params methods to tournaments controller to start validating and storing images new tournament registration

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,6 +3,30 @@ class TournamentsController < ApplicationController
     end
 
     def create
-      # do processing of form params
+    end
+
+    private 
+
+    def image_store_params
+      params.permit(
+                :logo,
+                :venue_logo,
+                :profile_picture
+      )
+    end
+
+    def tournament_params
+      # fields we want to perform any operations on on in this controller
+      params.permit(
+          :name,
+          :language,
+          :currency,
+          :details,
+          :venue_name,
+          :venue_address,
+          :venue_website,
+          :venue_contact,
+          :private_event
+      )
     end
 end
