145a89781109c0236cad4251a2a27ac96d77f413
abidsaif <saif.abid@mail.utoronto.ca>
2016-10-12 15:02:24 -0400

    Add validation and refactor create tournament form
    Validation on name, venue address and start date added.

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,86 +1,48 @@
 class TournamentsController < ApplicationController
-    def new
-      # If user isnt logged in dont let him enter the form
-    end
+  before_action :check_user_auth, only: [:new, :update, :destroy, :edit, :create]
 
-    def show
-      # 3 cases of users.
-      # 1. Organizer
-      # 2. Attendee 
-      # 3. Not logged in. Can log in as organizer or attendee
-      return self.error() unless Tournament.exists?(id: params[:id])
-      @tournament = Tournament.find(params[:id])
-      if session["warden.user.user.key"]
-        @session_user = User.find(session["warden.user.user.key"][0][0])
-        @is_organizer = Person.where(tournament_id: params[:id]).where(user_id: @session_user.id).where(is_organizer: 1).exists?
-        # ToDo: Implement when available
-        #@first_name = Account.where(user_id: @session_user.id)
-        @first_name = 'Leroy Jenkins'
-      else
-        @session_user = 'none'
-        @is_organizer = false
-        @first_name = 'none'
-      end
-      # ToDo: Update website URL in tournaments table: venue_website
+  def check_user_auth
+    redirect_to "/users/sign_in", notice: "Please login before creating a new tournament" unless user_signed_in?
+  end
 
-    end
+  def new
+    @tournament = Tournament.new
+  end
 
-    def create
-      # Confirm user is logged in
-      return self.error() unless session["warden.user.user.key"]
-      @user = User.find(session["warden.user.user.key"][0][0])
-      return self.error() unless @user
-
-      # Store images. If no images provided, it isn't an error. Continue storing data
-      logo_url = Image.store(:logo, image_store_params[:logo])
-      venue_logo_url = Image.store(:venue_logo, image_store_params[:venue_logo])
-      profile_picture_url = Image.store(:tournament_profile_picture, image_store_params[:profile_picture])
-      @tournament = Tournament.new({:name => tournament_params[:name],
-                                    :language => tournament_params[:language],
-                                    :currency => tournament_params[:currency],
-                                    :details => tournament_params[:details],
-                                    :venue_name => tournament_params[:venue_name],
-                                    :venue_address => tournament_params[:venue_address],
-                                    :venue_website => tournament_params[:venue_website],
-                                    :venue_contact_details => tournament_params[:venue_contact],
-                                    :is_private => tournament_params[:private_event],
-                                    :logo => logo_url,
-                                    :profile_pictures => [profile_picture_url],
-                                    :venue_logo => venue_logo_url,
-                                    :start_date => tournament_params[:start_date]
-                                   })
-
-      return self.error() unless @tournament.register
-      return self.error() unless @tournament.person.new().insert_organizer @user.id
-      return self.success()
-    end
-
-    def image_store_params
-      params.permit(:logo, :venue_logo, :profile_picture)
-    end
-
-    # Placeholder to be replaced by success view
-    def success
-      redirect_to @tournament
-    end
-
-    # Placeholder to be replaced by failiure view
-    def error
+  def create
+    @tournament = Tournament.new(tournament_params)
+    @tournament.create_tournament
+    if (@tournament.errors.any?)
       render :new
+      return
     end
-    def tournament_params
-      # fields we want to perform any operations on on in this controller
-      params.permit(
-          :name,
-          :language,
-          :currency,
-          :details,
-          :venue_name,
-          :venue_address,
-          :venue_website,
-          :venue_contact,
-          :private_event,
-          :start_date
-      )
+
+    @tournament.person.new.insert_organizer current_user.id
+    if (@tournament.errors.any?)
+      render :new
+      return
     end
+
+    render :new # TODO: change this to success view
+  end
+
+
+  def tournament_params
+    # fields we want to perform any operations on on in this controller
+    params.require(:tournament).permit(
+        :name,
+        :language,
+        :currency,
+        :details,
+        :venue_name,
+        :venue_address,
+        :venue_website,
+        :venue_contact_details,
+        :is_private,
+        :start_date,
+        :logo_data,
+        :venue_logo_data,
+        :profile_picture_data
+    )
+  end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,6 +3,22 @@ class Tournament < ApplicationRecord
   has_many :tournament_tickets, dependent: :destroy
   has_many :person
 
+  validates_presence_of :name, :venue_address, :start_date
+  validate :start_date_is_not_past
+
+  def start_date_is_not_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  # Functionality used by mapping view in welcome/hello_world
+  geocoded_by :venue_address
+  after_validation :geocode
+
+  # Functionality used by location sorting view in welcome/hello_world
+  acts_as_mappable :distance_field_name => :venue_address, :lat_column_name => :latitude, :lng_column_name => :longitude
+
   @language_options = [
       ['English', 'english'],
       ['French', 'french']
@@ -21,16 +37,12 @@ class Tournament < ApplicationRecord
     @currency_options
   end
 
-  # Functionality used by mapping view in welcome/hello_world
-  geocoded_by :venue_address
-  after_validation :geocode
-  
-  # Functionality used by location sorting view in welcome/hello_world
-  acts_as_mappable :distance_field_name => :venue_address, :lat_column_name => :latitude, :lng_column_name => :longitude
 
-  # Register a new tournament
-  def register
+  # create_tournament creates a new tournament
+  def create_tournament
+    self.logo = Image.store(:logo, self.logo)
+    self.venue_logo = Image.store(:venue_logo, self.venue_logo)
+    self.profile_pictures = Image.store(:profile_pictures, self.profile_pictures)
     self.save
-    return true
   end
 end
