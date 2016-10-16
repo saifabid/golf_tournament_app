b1bbc363ddc0f91c3d790da92c26cffd4eac76b5
abidsaif <saif.abid@mail.utoronto.ca>
2016-10-16 02:01:51 -0400

    Add back show action in tournaments controller whcih was deleted earlier

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -10,7 +10,7 @@ class TournamentsController < ApplicationController
   def create
     @tournament = Tournament.new(tournament_params)
     @tournament.create_tournament
-    if (@tournament.errors.any?)
+    if (@tournament.errors.any?)   
       render :new
       return
     end
@@ -24,23 +24,44 @@ class TournamentsController < ApplicationController
     render :new # TODO: change this to success view
   end
 
+  def show
+    @tournament = Tournament.find(params[:id])
+    if @tournament.errors.any?
+      render :new
+      return 
+    end 
 
+    # 3 cases of users.
+    # 1. Organizer
+    # 2. Attendee 
+    # 3. Not logged in. Can log in as organizer or attendee
+    if user_signed_in?
+      @session_user = current_user
+      @is_organizer = Person.where(tournament_id: params[:id]).where(user_id: current_user.id).where(is_organizer: 1).exists?
+      @first_name = 'Leroy Jenkins'
+    else 
+      @session_user = 'none'
+      @is_organizer = false
+      @first_name = 'none'
+    end  
+   end 
+   
   def tournament_params
     # fields we want to perform any operations on on in this controller
     params.require(:tournament).permit(
-        :name,
-        :language,
-        :currency,
-        :details,
-        :venue_name,
-        :venue_address,
-        :venue_website,
-        :venue_contact_details,
-        :is_private,
-        :start_date,
-        :logo_data,
-        :venue_logo_data,
-        :profile_picture_data
+      :name,
+      :language,
+      :currency,
+      :details,
+      :venue_name,
+      :venue_address,
+      :venue_website,
+      :venue_contact_details,
+      :is_private,
+      :start_date,
+      :logo_data,
+      :venue_logo_data,
+      :profile_picture_data
     )
   end
 end
