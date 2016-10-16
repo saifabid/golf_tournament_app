3ddc5099854dd40c0ff7e11d7fbf1b0fcc2cb7fa
Wei Chung Yang <willy.yang@mail.utoronto.ca>
2016-10-16 16:59:22 -0400

    Organized routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
+  resources :users
+  resources :tournaments do
+    resources :tournament_events
+    resources :tournament_tickets
+  end
+  resources :signup
+  resources :charges
+
   get 'credit_card/index'
 
   get 'about/contact'
@@ -18,10 +28,26 @@ Rails.application.routes.draw do
   get 'legal/privacy'
 
   post 'welcome/hello_world'
+
   get "welcome/hello_world" => "welcome#hello_world"
+  
+  post 'users/edit' => 'accounts#create'
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  get 'users/account'
 
+  get 'users/forgot_password'
+
+  post 'users/forgot_password'
+
+  get 'tournaments/:id', to: 'tournaments#show'
+
+  get 'tournament_stats/:id', to: 'tournament_stats#show'
+
+  get 'tournaments/:id/edit', to: 'tournaments#edit'
+
+  get 'dashboard/index'
+
+  root to: 'welcome#hello_world'
   # authenticated :user do
   #   root 'users#account', as: :authenticated_root
   # end
@@ -41,31 +67,5 @@ Rails.application.routes.draw do
 
   # get 'tournament_registration/index'
 
-  post 'users/edit' => 'accounts#create'
-
-  get 'users/account'
-
-  get 'users/forgot_password'
-
-  post 'users/forgot_password'
-
-  resources :users
-  resources :tournaments do
-    resources :tournament_events
-    resources :tournament_tickets
-  end
-  resources :signup
-
-  resources :charges
-  # resource :tournies
-
-  get 'tournaments/:id', to: 'tournaments#show'
-  get 'tournament_stats/:id', to: 'tournament_stats#show'
-  get 'tournaments/:id/edit', to: 'tournaments#edit'
-
-  get 'dashboard/index'
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'welcome#hello_world'
 end
