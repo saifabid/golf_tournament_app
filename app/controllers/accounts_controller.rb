bca1aa54f83ac803fd9c1b1528c16033a964cc60
vijayvai <vaibhav.vijay@mail.utoronto.ca>
2016-10-21 06:24:34 -0400

    Showing all the account details of a user, changed routes for editing account details. Profile Pricture being seen and can be changed. Vaibhav Vijay and Pranav Mehndiratta

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,11 +3,15 @@ class AccountsController < ApplicationController
     @user = User.find(current_user.id)
     @account = @user.account.new account_params
     if @account.save
-    	redirect_to root
+    	redirect_to '/'
     else
     	redirect_to edit_user_registration
+    end
   end
-
+  def update
+    puts("helloklsldjfklsdfjkdslfjdkslfdslkfjdsklfjdkslfjdsklfjdkslfjdsklfjdsklfjdsklj")
+  end
+  private
    def account_params
      params.require(:account).permit(:user_id, :avatar, :prefix, :first_name, :middle_name, :last_name, :suffix, :home_adr1, :home_adr2, :home_city, :home_province, :home_code, :home_country, :bill_adr1, :bill_adr2, :bill_city, :bill_province, :bill_code, :bill_country, :gender, :birth, :mobile_phone, :is_home)
    	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,13 @@ class UsersController < ApplicationController
     #   puts("false")
     # end
 
-    render plain: params[:user].inspect
+    # render plain: params[:user].inspect
   end
 
   def show
     @user = User.find(current_user.id)
     # puts(@user.account.)
-    @account = @user.account.find(current_user.id)
+    @account = Account.find(current_user.id)
     # puts(@account.first_name)
     # @data = params[:account]
     # before_action :authenticate_user
@@ -35,16 +35,21 @@ class UsersController < ApplicationController
 
   def update
     # before_action :authenticate_user
+    @user = User.find(current_user.id)
+    @account = Account.find(current_user.id)
+    if @account.update(account_params)
+      redirect_to @user
+    else
+      redirect_to edit_user_registration
+    end
   end
 
   def destroy
     # before_action :authenticate_user
   end
 
-  # def signin 
-  # end
-
-  # def fp
-  #   # render plain: params[:forgot_password].inspect
-  # end
+  private
+    def account_params
+      params.require(:account).permit(:user_id, :avatar, :prefix, :first_name, :middle_name, :last_name, :suffix, :home_adr1, :home_adr2, :home_city, :home_province, :home_code, :home_country, :bill_adr1, :bill_adr2, :bill_city, :bill_province, :bill_code, :bill_country, :gender, :birth, :mobile_phone, :is_home)
+    end
 end
