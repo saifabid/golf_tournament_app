249a6589c3f473ea91bb8803d92e1261bd92cb1e
abidsaif <saif.abid@mail.utoronto.ca>
2016-10-22 01:52:09 -0400

    Add functionality so once a tournament is created organizer is taken to page to create agenda
    - also include a new datetime/time picket for tournament creation and agenda creation which should make it a bit easier to pick times

--- app/controllers/tournament_events_controller.rb
+++ app/controllers/tournament_events_controller.rb
@@ -2,15 +2,31 @@ class TournamentEventsController < ApplicationController
   before_action :check_user_auth
   def index
     @tournament= Tournament.find(params[:tournament_id])
+    @tournament.tournament_events.build
   end
+
   def create
       @tournament= Tournament.find(params[:tournament_id])
+      if @tournament.errors.any?
+        flash[:error] = @tournament.errors.full_messages.to_sentence
+        puts flash[:error]
+        render :index
+        return
+      end
+
       @tournament_event= @tournament.tournament_events.create(tournament_event_params)
+      if @tournament_event.errors.any?
+        flash[:error] = @tournament_event.errors.full_messages.to_sentence
+        puts flash[:error]
+        render :index
+        return
+      end
+
       redirect_to tournament_tournament_events_path(@tournament)
   end
 
   private
   def tournament_event_params
-  params.require(:tournament_event).permit(:event_name,:start_time, :end_time)
+  params.require(:tournament_event).permit(:event_name, :start_time, :end_time, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020155646) do
+ActiveRecord::Schema.define(version: 20161022050136) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -33,10 +33,14 @@ ActiveRecord::Schema.define(version: 20161020155646) do
     t.string   "bill_province"
     t.boolean  "gender"
     t.date     "birth"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "mobile_phone"
     t.boolean  "is_home"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
   end
 
@@ -80,6 +84,7 @@ ActiveRecord::Schema.define(version: 20161020155646) do
     t.time     "end_time"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "description"
     t.index ["tournament_id"], name: "index_tournament_events_on_tournament_id", using: :btree
   end
 
