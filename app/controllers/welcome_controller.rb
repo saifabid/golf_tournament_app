dee64a2c81275eec2b59c71961f4566c75f88ab8
iddom <iddo.messinger@mail.utoronto.ca>
2016-10-26 03:54:26 -0400

    search improvement prelim

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,24 @@
 require 'date'
+#include Geokit::Geocoders
 
 class WelcomeController < ApplicationController
   def hello_world
-  	sortType = params[:q]
+  	
+    #url search params
+    sortType = params[:q]
+    title = ActiveRecord::Base::sanitize(params[:searchTitle])
+    searchStart = ActiveRecord::Base::sanitize(params[:searchStart])
+    searchEnd = ActiveRecord::Base::sanitize(params[:searchEnd])
+    searchDistance = ActiveRecord::Base::sanitize(params[:searchDistance])
+    clientIp = ActiveRecord::Base::sanitize(params[:client_ip])
+
+    #puts sortType
+    #puts title
+    #puts searchStart
+    #puts searchEnd
+    #puts searchDistance
+    #puts clientIp
+
 
   	@events = Tournament.where("start_date >= NOW()").where("is_private = '0'")
     @hash = Gmaps4rails.build_markers(@events) do |event, marker|
@@ -10,6 +26,19 @@ class WelcomeController < ApplicationController
       marker.lng event.longitude
       marker.infowindow event.details
     end
+    
+    #puts MultiGeocoder.geocode('12.215.42.19') 
+
+    #doesn't work! IP of service is dead. Need to find alternative
+    #look inside of /config/initializers/geokit_config.rb , setup google api keys???
+
+    #relevant error:
+    #Ip geocoding. address: 12.215.42.19, args []
+    #C:/Ruby23/lib/ruby/gems/2.3.0/gems/geokit-1.10.0/lib/geokit/geocoders.rb:141: wa
+    #rning: constant Geokit::Geocoders::Geocoder::TimeoutError is deprecated
+    #Caught an error during Ip geocoding call: Failed to open TCP connection to api.h
+    #ostip.info:80 (getaddrinfo: No such host is known. )
+    
     
     # https://github.com/geokit/geokit-rails
     # Caveat notes that not able to do a where clause, therefore can't get descending locations
@@ -21,7 +50,7 @@ class WelcomeController < ApplicationController
 
   	@state = params[:state]
   	if sortType == "1"
-	  	@tournaments = Tournament.where("start_date >= NOW()").where("is_private = '0'").order(start_date: :asc)
+	  	@tournaments = Tournament.where("start_date >= ?", searchStart).where("is_private = '0'").order(start_date: :asc)
   	elsif sortType == "2"
   		@tournaments = Tournament.where("start_date >= NOW()").where("is_private = '0'").order(start_date: :desc)
   	elsif sortType == "3"
@@ -30,7 +59,8 @@ class WelcomeController < ApplicationController
   		@tournaments = Tournament.where("start_date >= NOW()").where("is_private = '0'").order(name: :desc)
     elsif sortType == "5"
       # ToDo: Implement sort by location
-      @tournaments = Tournament.within(0.5, :origin => '21 Carlton St, Toronto, ON')
+      #@tournaments = Tournament.within(0.5, :origin => '21 Carlton St, Toronto, ON')
+      @tournaments = Tournament.within(0.5, :origin => [43.6611,-79.3821])
   	else
 	  	@tournaments = Tournament.where("start_date >= NOW()").where("is_private = '0'").order(start_date: :asc)
 		end
