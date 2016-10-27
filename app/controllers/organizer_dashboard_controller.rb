5e6140b5f2100792a82a712361121ef534b22c98
kumares3 <seyan.kumaresan@mail.utoronto.ca>
2016-10-27 19:52:04 -0400

    Added route back to tournaments page when user tries to access organiser dashboard when not organiser

--- app/controllers/organizer_dashboard_controller.rb
+++ app/controllers/organizer_dashboard_controller.rb
@@ -1,4 +1,14 @@
 class OrganizerDashboardController < ApplicationController
+
+  before_action :check_tournament_organizer, only: [:show]
+
+  def check_tournament_organizer
+    if !Person.where(sprintf("user_id = %d AND tournament_id = %d AND is_organizer = 1", current_user.id, params[:id])).exists?
+      redirect_to sprintf("/tournaments/%s", params[:id])
+      return
+    end
+  end
+
   def show
 
   end
