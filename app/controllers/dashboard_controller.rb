4b5c1db1e5436a8c12e8302856121eb42a46e1b1
guoshen2 <tonyguo526@hotmail.com>
2016-10-31 22:36:16 -0400

    Added My Created Tournaments and My Participating Tournaments to Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class DashboardController < ApplicationController
+  before_action :check_user_auth
   def index
+    @participatingtournaments= getpartipatingtournaments
+    @createdtournaments= getcreatedtournaments
+  end
+  def getpartipatingtournaments
+    userid= current_user.id
+    return Tournament.joins(:people).where(:people => {:user_id=> userid, :is_player=>1, :is_guest=>0} )
 
+  end
+  def getcreatedtournaments
+    userid= current_user.id
+    return Tournament.joins(:people).where(:people=> {:user_id=> userid, :is_organizer=> 1})
   end
 end
