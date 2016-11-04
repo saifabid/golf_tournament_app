f5789709efcc0ad90eed7c6fde8ef57b81954777
MingCHsu <ming.hsu95@gmail.com>
2016-11-03 23:14:23 -0400

    Added in payments to stripe account, can use stripe without typing in publishable keys now

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,23 +6,26 @@ class ChargesController < ApplicationController
    end
 
    def create
-     #getting transaction_id
-	     
-
      # Amount in cents
      @amount = 100
 
-     customer = Stripe::Customer.create(
-       :email => params[:stripeEmail],
-       :source  => params[:stripeToken]
-     )
+	Stripe.api_key = "sk_test_jZCX7Zq0KZFYjGAVtOhc58io"
+
+	# Get the credit card details submitted by the form
+
+     #customer = Stripe::Customer.create(
+     #   :email => params[:stripeEmail],
+     #   :source  => params[:stripeToken]
+     #)
 
      charge = Stripe::Charge.create(
-       :customer    => customer.id,
-       :amount      => @amount,
-       :description => 'Rails Stripe customer',
-       :currency    => 'usd'
-     )
+        #:customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+	:source => params[:stripeToken],
+        :currency    => 'cad'
+     ) 
+
 
    rescue Stripe::CardError => e
      flash[:error] = e.message
