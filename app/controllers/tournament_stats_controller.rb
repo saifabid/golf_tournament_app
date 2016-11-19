7fd3b53ce577211ea0929e2adc6b6ef998d418f4
Pranav Mehndiratta <pranav.mehndiratta@mail.utoronto.ca>
2016-11-19 06:14:26 -0500

    Event stats. - Pranav Mehndiratta and Vaibhav Vijay

--- app/controllers/tournament_stats_controller.rb
+++ app/controllers/tournament_stats_controller.rb
@@ -1,9 +1,43 @@
 class TournamentStatsController < ApplicationController
+  # before_action :check_tournament_organizer
+
   def show
   	# Access info and stats on tournament (ie # of players signed up, amount of revenue generated etc.)
   	# ToDo: Read from signups 
   	@id = params[:id]
-  	@tournament_name = Tournament.find(params[:id])
-  	@tournament_name = @tournament_name.name
+  	@tournament = Tournament.find(params[:id])
+  	@tournament_name = @tournament.name
+
+  	@num_checked_in = 0
+  	Person.where(tournament_id: @id, checked_in: true).find_each do |person|
+  	  @num_checked_in += 1
+  	end
+
+  	@player_tickets_left = @tournament.tickets_left
+    @spectator_tickets_left = @tournament.spectator_tickets_left
+    @dinner_tickets_left = @tournament.dinner_tickets_left
+
+    #TODO: adjust revenue_players for foursome price
+    @revenue_players = (@tournament.total_player_tickets - @player_tickets_left) * @tournament.player_price
+    @revenue_spectators = (@tournament.total_audience_tickets - @spectator_tickets_left) * @tournament.spectator_price
+    @revenue_dinner = (@tournament.total_dinner_tickets - @dinner_tickets_left) * @tournament.dinner_price
+  	@revenue_sponsors = 0
+    TournamentSponsorship.where(tournament_id: @id).find_each do |sponsor|
+  	  @revenue_sponsors += sponsor.ticket_price
+  	end
+    @revenue = @revenue_players + @revenue_spectators + @revenue_dinner + @revenue_sponsors
+
+    @potential_revenue_players = @player_tickets_left * @tournament.player_price
+    @potential_revenue_spectators = @spectator_tickets_left * @tournament.spectator_price
+    @potential_revenue_dinner = @dinner_tickets_left * @tournament.dinner_price
+    @potential_revenue = @potential_revenue_players + @potential_revenue_spectators + @potential_revenue_dinner
+
+    @cost = (@tournament.total_player_tickets - @player_tickets_left) * 2.5
+
+    @num_guests = 0
+    Person.where(tournament_id: @id, is_guest: true).find_each do |person|
+  	  @num_guests += 1
+  	end
+
   end
  end
