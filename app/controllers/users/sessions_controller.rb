90ee432b6dd879307236b9fa45e06c72e6d9c4aa
Wei Chung Yang <willy.yang@mail.utoronto.ca>
2016-11-19 20:08:05 -0500

    Fix BUG: _link_back_to_event_page uses session[:tournament_id] . This causes issues when the session is holding an old value

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,9 +12,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    session.delete(:tournament_id)
+    super
+  end
 
   # protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'tournament_list/list'
   post 'tournament_list/list'
 
-  devise_for :users, :controllers => { :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :registrations => "users/registrations", :sessions => "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users
   post 'users/new' => "users#create"
