7ba10e97df05fc2ed27426dcdf7519d09d4f17fe
Pranav Mehndiratta <pranav.mehndiratta@mail.utoronto.ca>
2016-11-20 23:44:31 -0500

    Fixed logic in tournaments stat controller for number of player tickets sold. Removed facebook and google plus like buttons. - Pranav Mehndiratta and Vaibhav Vijay

--- app/controllers/tournament_stats_controller.rb
+++ app/controllers/tournament_stats_controller.rb
@@ -14,22 +14,18 @@ class TournamentStatsController < ApplicationController
   	end
 
   	@player_tickets_left = @tournament.tickets_left
-  	@player_tickets_sold = @tournament.total_player_tickets - @player_tickets_left - 4 * @tournament.num_foursomes
+  	@player_tickets_sold = @tournament.total_player_tickets - @player_tickets_left
     @spectator_tickets_left = @tournament.spectator_tickets_left
     @spectator_tickets_sold = @tournament.total_audience_tickets - @spectator_tickets_left
     @dinner_tickets_left = @tournament.dinner_tickets_left
     @dinner_tickets_sold = @tournament.total_dinner_tickets - @dinner_tickets_left
 
     #TODO: adjust revenue_players for foursome price
-    @revenue_players = @player_tickets_sold * @tournament.player_price
+    @revenue_players = (@player_tickets_sold - 4 * @tournament.num_foursomes) * @tournament.player_price
     @revenue_foursomes = @tournament.num_foursomes * @tournament.foursome_price
     @revenue_spectators = @spectator_tickets_sold * @tournament.spectator_price
     @revenue_dinner = @dinner_tickets_sold * @tournament.dinner_price
   	
-  	@num_sponsors = 0
-    Person.where(tournament_id: @id, is_sponsor: true).find_each do |sponsor|
-  	  @num_sponsors += 1
-  	end
     @revenue = @revenue_players + @revenue_foursomes + @revenue_spectators + @revenue_dinner
 
     @potential_revenue_players = @player_tickets_left * @tournament.player_price
@@ -38,6 +34,13 @@ class TournamentStatsController < ApplicationController
     @potential_revenue = @potential_revenue_players + @potential_revenue_spectators + @potential_revenue_dinner
 
     @cost = (@tournament.total_player_tickets - @player_tickets_left) * 2.5
+
+    @net = @revenue - @cost
+
+  	@num_sponsors = 0
+    Person.where(tournament_id: @id, is_sponsor: true).find_each do |sponsor|
+  	  @num_sponsors += 1
+  	end
 
     @num_guests = 0
     Person.where(tournament_id: @id, is_guest: true).find_each do |person|
