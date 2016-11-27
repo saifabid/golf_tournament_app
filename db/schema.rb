a1cb86941b51705e200181a12577ad7c4f7d2945
kumares3 <seyan.kumaresan@mail.utoronto.ca>
2016-11-27 16:11:10 -0500

    Added card surcharge field to ticket_transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127205125) do
+ActiveRecord::Schema.define(version: 20161127210833) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20161127205125) do
     t.bigint   "transaction_number"
     t.integer  "user_id"
     t.decimal  "amount_paid",        precision: 16, scale: 2
+    t.decimal  "card_surcharge",     precision: 16, scale: 2
     t.datetime "created_at",                                                  null: false
     t.datetime "updated_at",                                                  null: false
     t.string   "currency",                                    default: "cad", null: false
