4df952fc1e9687047fabd0e497f673e168d387ef
kumares3 <seyan.kumaresan@mail.utoronto.ca>
2016-12-01 13:25:08 -0500

    Updated routes to help with organizer dashboard routing error

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20161130231710) do
     t.integer  "member_four"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.time     "start"
+    t.time     "end"
   end
 
   create_table "people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
