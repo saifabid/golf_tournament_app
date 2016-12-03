a6aa60f6e7d99e6858cae992f98fa22bd1207b58
guoshen2 <tonyguo526@hotmail.com>
2016-12-03 14:16:48 -0500

    fix bug duplicating dashboard calendar

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+    $('#dashboardCalendar').fullCalendar({
+        header: {
+            left: 'prev,next',
+            center: 'title'
+        },
+        eventSources: [
+            {
+                url: 'participatingtournaments_feed',
+                color: 'blue',
+                textColor: 'white'
+            },
+            {
+                url: 'createdtournaments_feed',
+                color: 'green',
+                textColor: 'white'
+            },
+            {
+                url: 'spectatortournaments_feed',
+                color: 'purple',
+                textColor: 'white'
+            },
+            {
+                url:'sponsoredtournaments_feed',
+                color: 'orange',
+                textcolor: 'white'
+            }
+        ]
+
+    });
+});
